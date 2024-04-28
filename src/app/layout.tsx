872319e7d4d6653350acc6e1e02d03f92950325f
Andrew Bwogi <andrew.bwogi@gmail.com>
2024-04-28 04:17:35 -0700

    title adjustments in the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,10 @@ import { type Metadata } from 'next'
 export const metadata: Metadata = {
   title: {
     template: '%s - TaxPal',
-    default: 'TaxPal - Accounting made simple for small businesses',
+    default: 'OdeLabs - Finer computing experiments',
   },
   description:
-    'Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited.',
+    'Transforming ideas by unlocking digital possibilities through finely crafted experiments into exceptional web experiences.',
 }
 
 const inter = Inter({
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ export function Hero() {
   return (
     <Container className="pb-16 pt-20 text-center lg:pt-32">
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-        endless{' '}
+        Endless{' '}
         <span className="relative whitespace-nowrap text-[#064BAC]">
           <svg
             aria-hidden="true"
